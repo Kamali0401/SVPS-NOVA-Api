d7071f0125e5b5c7f78759146a977de6a4f8c359
nanthinik30@gmail.com <nanthinik30@gmail.com>
2025-09-01 18:00:49 +0530

    report .xlsxn bin

--- RestaurantManagement.Application/obj/Debug/net8.0/SonaNova.Application.AssemblyInfo.cs
+++ RestaurantManagement.Application/obj/Debug/net8.0/SonaNova.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SonaNova.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f8c787097adfceadc14dd71228ea6c08ffa815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14f57313e24a4db616d5b4e0e7cfd8e783bf6c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SonaNova.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SonaNova.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RestaurantManagement.Infrastructure/obj/Release/net8.0/SonaNova.Infrastructure.AssemblyInfo.cs
+++ RestaurantManagement.Infrastructure/obj/Release/net8.0/SonaNova.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SonaNova.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba7f8497948ff80955af92937c61f34feecf900")]
 [assembly: System.Reflection.AssemblyProductAttribute("SonaNova.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SonaNova.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
